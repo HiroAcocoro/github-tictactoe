9a6d446ecaba92ee7354e697b7b15d13b9f1982b
Hiroshi Acocoro <57206370+HiroAcocoro@users.noreply.github.com>
2024-04-24 09:55:57 +0800

    fix msg

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	// constants
 	var moves = [...]string{"c1", "a1", "b3", "b2"}
 	var emptyBoard string = "||a|b|c|\n|---|---|---|---|\n|1|⬛|⬛|⬛|\n|2|⬛|⬛|⬛|\n|3|⬛|⬛|⬛|"
-	var templateMsg string = "# GITHUB IS A GAME ENGINE\n\n### PR a new move inside moves.go > Moves\n\n### ToDo\n- Github actions to set and render your moves\n- Move validations\n- Win game scenario\n\n\n# CURRENT GAME:\n\n\n"
+	var templateMsg string = "# GITHUB IS A GAME ENGINE\n\n### PR a new move inside main.go > moves\n\n### ToDo\n- Github actions to set and render your moves\n- Move validations\n- Win game scenario\n\n\n# CURRENT GAME:\n\n\n"
 
 	// 27 is the start of the Board
 	var renderBoard string = emptyBoard
